d6cbdc6997d080af7ade6b5883c0d1a7d8a6f18b
Admin <Admin@192.168.100.2>
2017-05-01 23:10:57 +0530

    Dev1-added new class

---
+++ ExampleProj1/src/com/jvr/dev1/ArithmeticOperation.java
@@ -0,0 +1,9 @@
+package com.jvr.dev1;
+
+public class ArithmeticOperation {
+
+ public int sum(int a,int b){
+	 return a+b;
+ }	
+	
+}
--- ExampleProj1/src/com/jvr/gf/Example.java
+++ ExampleProj1/src/com/jvr/gf/Example.java
@@ -1,10 +1,15 @@
 package com.jvr.gf;
 
+import com.jvr.dev1.ArithmeticOperation;
+
 public class Example {
 
 	public static void main(String[] args) {
 		
     System.out.println("Hello-Git");
+	ArithmeticOperation aop=new ArithmeticOperation();
+	int sum=aop.sum(200,400);
+	System.out.println("Sum::"+sum);
 	}//main
 
 }//class
